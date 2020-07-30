0d06b5e70890b82654a9ec8738076358087f37dc
ayushbhardwaj307 <54473575+ayushbhardwaj307@users.noreply.github.com>
2020-07-30 19:17:27 +0530

    first commit

---
+++ src/main/java/com/upgrad/ideaApp/IdeaAppApplication.java
@@ -0,0 +1,15 @@
+package com.upgrad.ideaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+		SpringApplication.run(IdeaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upgrad/ideaApp/IdeaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upgrad.ideaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
